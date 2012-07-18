36daed5e4c0d6ce4d9a8800a153f7c56d578f5ff
Nick <nv4@rice.edu>
2012-07-19 00:01:57 +0800

    Thru 32

--- src/project_euler/problems_040.clj
+++ src/project_euler/problems_040.clj
@@ -1,5 +1,6 @@
 (ns project-euler.problems-030
-  (:use project-euler.core))
+  (:use project-euler.core)
+  (:use [clojure.math.combinatorics :only (permutations combinations)]))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Problem 031
@@ -13,4 +14,32 @@
        (->> (range 0 (inc remainder) coin)
             (map #(euler-031 coins (- remainder %)))
             (reduce +)))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Problem 032
+
+(defn seq2num [ds]
+  (reduce (fn [acc d] (+ (* 10 acc) d)) ds))
+
+(defn num2seq [n]
+  (loop [n n, acc ()]
+    (if (zero? n) acc
+      (recur (quot n 10) (conj acc (rem n 10))))))
+
+(defn digit-triplets [ds dset]
+  (let [others (sort (apply disj dset ds))]
+    (for [i (range 1 (count ds))]
+      (conj (->> ds (split-at i) (map seq2num) vec) others))))
+
+(defn euler-032
+  "Find the sum of all numbers that can be written as pandigital products with digits 1-9."
+  [] (let [ds (range 1 10), dset (set ds)]
+       (->> (mapcat #(combinations ds %) [4 5])
+            (mapcat permutations)
+            (mapcat #(digit-triplets % dset))
+            (keep (fn [[x y z]]
+                    (let [p (* x y)]
+                      (if (= z (-> p num2seq sort)) p))))
+            distinct
+            (reduce +))))
 
