18584f6b7bd8603aadb749d8610bdf8a68d63f66
Nick <nv4@rice.edu>
2012-07-18 18:08:25 +0800

    Thru 31

--- src/project_euler/core.clj
+++ src/project_euler/core.clj
@@ -4,8 +4,6 @@
 
 (defn divides? [x y] (zero? (mod x y)))
 
-(defn prime? [n] (= n (first (drop-while #(< n %) primes))))
-
 (defn chr2int [c] (- (int c) 48))
 
 (def primes
@@ -16,6 +14,8 @@
            (if (not-any? #(divides? n %) (take i primes))
              (cons n (p (inc i) (+ 2 n)))
              (recur (+ 2 n)))))) 1 3)))
+
+(defn prime? [n] (= n (first (drop-while #(< n %) primes))))
 
 (defn naturals [] (drop 1 (range)))
 
@@ -29,4 +29,9 @@
 (defn fib
   ([] (fib 0 1))
   ([x y] (lazy-seq (cons y (fib y (+' x y))))))
+
+(defn up-to
+  ([z]   (up-to 0 z))
+  ([a z] (up-to a z 1))
+  ([a z i] (range a (inc z) i)))
 
--- src/project_euler/problems_030.clj
+++ src/project_euler/problems_030.clj
@@ -1,6 +1,6 @@
 (ns project-euler.problems-030
   (:use project-euler.core)
-  (:use [clojure.math.combinatorics :only (permutations)]))
+  (:use clojure.math.combinatorics))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Problem 021
@@ -134,5 +134,17 @@
           (filter #(apply = %))
           (map first)
           (reduce +))))
-(euler-030)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Problem 031
+
+(defn euler-031
+  "How many different ways can 200p be made using any number of coins?
+   (coins: 1p, 2p, 5p, 10p, 20p, 50p, 100p and 200p)"
+  ([] (euler-031 [200 100 50 20 10 5 2] 200))
+  ([[coin & coins] remainder]
+     (if-not coin 1
+       (->> (range 0 (inc remainder) coin)
+            (map #(euler-031 coins (- remainder %)))
+            (reduce +)))))
 
