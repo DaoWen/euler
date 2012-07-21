6d79003a57835fd1e1b36c2343dc71659b5cb4cc
Nick <nv4@rice.edu>
2012-07-21 17:04:13 +0800

    Thru 43

--- src/project_euler/problems_050.clj
+++ src/project_euler/problems_050.clj
@@ -3,10 +3,10 @@
   (:use [clojure.math.combinatorics :only (permutations)]))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Problem 051
+;; Problem 041
 
-(defn euler-051 "What is the largest n-digit pandigital prime that exists?"
-  ([] (euler-051 9))
+(defn euler-041 "What is the largest n-digit pandigital prime that exists?"
+  ([] (euler-041 9))
   ([n] (->> (for [i (range 2 (inc n))]
               (->> (range 1 i)
                    permutations
@@ -16,14 +16,31 @@
             (reduce max))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Problem 052
+;; Problem 042
 
-(defn tri-num? "Checks if the given integer is a triangle number." [t]
-  (and (pos? t) (zero? (rem (/ (- (Math/sqrt (+ 1 (* 8 t))) 1) 2) 1))))
+(defn tri-num? "Checks if the given integer is a triangle number."
+  [t] (and (pos? t) (zero? (rem (/ (- (Math/sqrt (+ 1 (* 8 t))) 1) 2) 1))))
 
-(defn euler-052 "How many are triangle words are in the given string?"
-  ([] (euler-052 (re-seq #"\w+" (slurp "data/p052.txt"))))
+(defn euler-042 "How many are triangle words are in the given string?"
+  ([] (euler-042 (re-seq #"\w+" (slurp "data/p042.txt"))))
   ([words] (reduce + (for [w words
                            :let [v (reduce + (map #(- (int %) 64) w))]
                            :when (tri-num? v)] 1))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Problem 043
+
+(defn euler-043
+  ([] (euler-043 (range 10) [1 2 3 5 7 11 13 17]))
+  ([ds xs] (->> (for [dseq (permutations ds)
+                      :let [dseq (vec dseq)]
+                      :when (and (pos? (dseq 0))
+                                 (even? (dseq 3))
+                                 (#{0 5} (dseq 5)))
+                      :when (->> (partition 3 1 dseq)
+                                 (map seq2num)
+                                 (map vector xs)
+                                 (every? (fn [[p d]] (divides? d p))))]
+                  (seq2num dseq))
+                (reduce +))))
 
